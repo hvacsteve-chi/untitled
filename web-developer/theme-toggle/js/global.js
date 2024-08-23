11df706a238775a26384f3300e8c89d7f4c332dc
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-08-23 16:01:29 -0500

    Update global.js

--- web-developer/theme-toggle/js/global.js
+++ web-developer/theme-toggle/js/global.js
@@ -1,2 +1,12 @@
 const rootHTML = document.documentElement;
 console.log(rootHTML);
+const rootBODY = document.body;
+console.log(rootBODY);
+const rootHEAD = document.head;
+console.log(rootHEAD);
+const rootTITLE = document.title;
+console.log(rootTITLE);
+const rootAPP = document.querySelector("APP");
+console.log(rootAPP);
+
+
